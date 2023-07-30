69c439ee1744855d7e10db8aed46225b058ce7e1
tingul4 <xpple413208@gmail.com>
2023-07-30 22:01:16 +0800

    feat: complete CRUD in todos routes

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -10,4 +10,49 @@ router.get('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/new', (req, res) => {
+  const userId = req.user.id
+  const name = req.body.name
+  return Todo.create({ name, userId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const userId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { id, userId } })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const userId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findOne({ where: { id, userId } })
+    .then(todo => {
+      todo.set({
+        name,
+        isDone: isDone === 'on'
+      })
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => {
+  const userId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { id, userId } })
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 module.exports = router
